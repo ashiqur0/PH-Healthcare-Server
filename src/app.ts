834a29ab21056dc42bad5c1dc2696b0e8322459a
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-04-14 22:24:16 +0600

    feat: add notFound middleware for handling 404 errors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express, { Application, Request, Response } from "express";
 import { IndexRoutes } from "./app/routes";
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
+import { notFound } from "./app/middleware/notFound";
 
 const app: Application = express();
-
 
 // Enable URL-encoded form data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(globalErrorHandler);
+app.use(notFound);
 
 // Routes
 app.use('/api/v1', IndexRoutes);
