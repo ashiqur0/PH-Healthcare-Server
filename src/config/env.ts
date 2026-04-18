2879209194d2ef7ca5be8ef06f7698b02e7d9f3a
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-04-18 14:56:38 +0600

    feat: add email sending functionality with nodemailer and update environment configuration

--- src/config/env.ts
+++ src/config/env.ts
@@ -15,6 +15,13 @@ interface EnvConfig {
     REFRESH_TOKEN_EXPIRES_IN: string;
     BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN: string;
     BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE: string;
+    EMAIL_SENDER: {
+        SMTP_USER: string;
+        SMTP_PASSWORD: string;
+        SMTP_HOST: string;
+        SMTP_PORT: string;
+        SMTP_FROM: string;
+    }
 }
 
 const loadEnvVariables = (): EnvConfig => {
@@ -29,7 +36,12 @@ const loadEnvVariables = (): EnvConfig => {
         'REFRESH_TOKEN_SECRET',
         'REFRESH_TOKEN_EXPIRES_IN',
         'BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN',
-        'BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE'
+        'BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE',
+        'EMAIL_SENDER_SMTP_USER',
+        'EMAIL_SENDER_SMTP_PASSWORD',
+        'EMAIL_SENDER_SMTP_HOST',
+        'EMAIL_SENDER_SMTP_PORT',
+        'EMAIL_SENDER_FROM'
     ];
 
     requiredEnvVariable.forEach((variable) => {
@@ -50,7 +62,14 @@ const loadEnvVariables = (): EnvConfig => {
         REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET as string,
         REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN as string,
         BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN: process.env.BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN as string,
-        BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE: process.env.BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE as string
+        BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE: process.env.BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE as string,
+        EMAIL_SENDER: {
+            SMTP_USER: process.env.EMAIL_SENDER_SMTP_USER as string,
+            SMTP_PASSWORD: process.env.EMAIL_SENDER_SMTP_PASSWORD as string,
+            SMTP_HOST: process.env.EMAIL_SENDER_SMTP_HOST as string,
+            SMTP_PORT: process.env.EMAIL_SENDER_SMTP_PORT as string,
+            SMTP_FROM: process.env.EMAIL_SENDER_SMTP_FROM as string
+        }
     };
 }
 
