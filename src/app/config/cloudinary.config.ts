7e007d324eec03ba2b63327348cd7556c675dd8e
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-04-20 19:04:08 +0600

    feat: add uploadFileToCloudinary function to handle file uploads to Cloudinary

--- src/app/config/cloudinary.config.ts
+++ src/app/config/cloudinary.config.ts
@@ -1,7 +1,8 @@
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary, UploadApiResponse } from 'cloudinary';
 import { envVars } from './env';
 import AppError from '../errorHelpers/AppError';
 import status from 'http-status';
+
 cloudinary.config({
     cloud_name: envVars.CLOUDINARY_CLOUD_NAME,
     api_key: envVars.CLOUDINARY_API_KEY,
@@ -9,6 +10,48 @@ cloudinary.config({
 });
 
 export const cloudinaryUpload = cloudinary;
+
+export const uploadFileToCloudinary = async (buffer: Buffer, fileName: string): Promise<UploadApiResponse> => {
+    if (!buffer || !fileName) {
+        throw new AppError(status.BAD_REQUEST, "file or file name is required to upload");
+    }
+
+    const extension = fileName.split('.').pop()?.toLowerCase();
+
+    const fileNameWithoutExtension = fileName
+        .split('.')
+        .slice(0, -1)
+        .join('.')
+        .toLowerCase()
+        .replace(/\s+/g, '-')
+        // eslint-disable-next-line no-useless-escape
+        .replace(/[^a-z0-9\-]/g, '');
+
+    const uniqueName = Math.random().toString(36).substring(2) +
+        '-' +
+        Date.now() +
+        '-' +
+        fileNameWithoutExtension;
+
+    const folder = extension === 'pdf' ? 'pdfs' : 'images';
+
+    return new Promise((resolve, reject) => {
+        cloudinary.uploader.upload_stream(
+            {
+                resource_type: "auto",
+                public_id: `ph-health-care/${folder}/${uniqueName}`,
+                folder: `ph-health-care/${folder}`,
+            },
+            (error, result) => {
+                if (error) {
+                    reject(new AppError(status.INTERNAL_SERVER_ERROR, "Failed to upload file to Cloudinary"));
+                } else {
+                    resolve(result as UploadApiResponse);
+                }
+            }
+        ).end(buffer);
+    });
+};
 
 export const deleteFileFromCloudinary = async (url: string) => {
     const regex = /\/v\d+\/(.+?)(?:\.[a-zA-Z0-9]+)+$/;
@@ -19,7 +62,7 @@ export const deleteFileFromCloudinary = async (url: string) => {
         try {
             const publicId = match[1];
             await cloudinary.uploader.destroy(publicId, { resource_type: "auto" });
-            
+
             console.log(`file ${publicId} deleted from cloudinary`);
         } catch (error) {
             console.log("Error deleting file from cloudinary", error);
