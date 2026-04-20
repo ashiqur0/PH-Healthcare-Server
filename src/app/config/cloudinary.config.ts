a69b840058ee77101a1fbbf160ef0078ce3b8b32
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-04-20 18:30:40 +0600

    feat: enhance global error handler to delete files from Cloudinary on error

--- src/app/config/cloudinary.config.ts
+++ src/app/config/cloudinary.config.ts
@@ -1,5 +1,7 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { envVars } from './env';
+import AppError from '../errorHelpers/AppError';
+import status from 'http-status';
 cloudinary.config({
     cloud_name: envVars.CLOUDINARY_CLOUD_NAME,
     api_key: envVars.CLOUDINARY_API_KEY,
@@ -7,3 +9,23 @@ cloudinary.config({
 });
 
 export const cloudinaryUpload = cloudinary;
+
+export const deleteFileFromCloudinary = async (url: string) => {
+    const regex = /\/v\d+\/(.+?)(?:\.[a-zA-Z0-9]+)+$/;
+
+    const match = url.match(regex);
+
+    if (match && match[1]) {
+        try {
+            const publicId = match[1];
+            await cloudinary.uploader.destroy(publicId, { resource_type: "auto" });
+            
+            console.log(`file ${publicId} deleted from cloudinary`);
+        } catch (error) {
+            console.log("Error deleting file from cloudinary", error);
+
+            throw new AppError(status.INTERNAL_SERVER_ERROR, "Failed to delete file from Cloudinary");
+        }
+
+    }
+}
--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -7,12 +7,26 @@ import z, { unknown } from "zod";
 import { TErrorResponse, TErrorSource } from "../interface/error.interface";
 import { handleZodError } from "../errorHelpers/handleZodError";
 import AppError from "../errorHelpers/AppError";
+import { deleteFileFromCloudinary } from "../config/cloudinary.config";
 
 // Global error handler
-export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+export const globalErrorHandler = async (err: any, req: Request, res: Response, next: NextFunction) => {
 
     if (envVars.NODE_ENV === 'development') {
         console.error("Error from Global Error Handler: ", err);
+    }
+
+    // delete single file from cloudinary
+    if (req.file) {
+        await deleteFileFromCloudinary(req.file.path);
+    }
+    // প্রয়োজনীয়তা: ইমেক আলাদাভাবে আপলোড হয়ে যায়। এক্ষেত্রে অন্যান্য ডকুমেন্ট যদি ডাটাবেসে আপডেট হতে সমস্যা হয় তাহলে এই গ্লোবাল এরর হ্যান্ডলার এ ধরা পড়বে এবং ক্লাউডিনারিতে আপলোড হয়য়া ইমেজ গুলো ডিলেট হয়ে যাবে।
+
+    // delete multiple files from cloudinary
+    if (req.files && Array.isArray(req.files) && req.files.length > 0) {
+        const imageUrls = req.files.map((file) => file.path);
+
+        await Promise.all(imageUrls.map(async (url) => deleteFileFromCloudinary(url)));
     }
 
     let errorSources: TErrorSource[] = [];
